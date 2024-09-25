69a5a066ab1529ac6508b0e3cfc23104d52e81ab
Warren Harper <wharper@pyroninc.com>
2024-09-25 07:41:59 -0400

    Add home-manager to nixos

--- flake.nix
+++ flake.nix
@@ -1,16 +1,36 @@
 {
-	description = "A simple NixOS Configuration";
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-	};
-	
-	outputs = {self, nixpkgs, ...}@inputs: {
-		nixosConfigurations.redwall = nixpkgs.lib.nixosSystem {
-			system = "x86_64-linux";
-			modules = [
-				./configuration.nix
-        ./home/default.nix
-			];
-		};
-	};
+  description = "A simple NixOS Configuration";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations.redwall = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.warren = import ./home/default.nix;
+            home-manager.extraSpecialArgs = {
+              username = "warren";
+            };
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
+      };
+    };
 }
