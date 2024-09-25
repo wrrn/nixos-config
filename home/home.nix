09cfad5a4f274b4f035fde428b456df891dca2bb
Warren Harper <wharper@pyroninc.com>
2024-09-25 08:12:12 -0400

    Fix syntax problem

--- home/home.nix
+++ home/home.nix
@@ -1,8 +1,15 @@
-{username, config, pkgs, ...}: {
+{
+  username,
+  config,
+  pkgs,
+  ...
+}:
+{
   home.username = "${username}";
   home.homeDirectory = "/home/${username}";
 
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
-}
 
+  home.stateVersion = "24.05";
+}
