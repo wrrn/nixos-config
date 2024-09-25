2cdfcca237480e05f9a173b972b65d0271deab9a
Warren Harper <wharper@pyroninc.com>
2024-09-25 15:46:01 -0400

    Fix syntax problem

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     }@inputs:
     let
       system = "x86_64-linux";
-      dotfiles = ${dotfiles}.packages.${system};
+      dotfiles = {dotfiles}.packages.${system};
     in
     {
       nixosConfigurations.redwall = nixpkgs.lib.nixosSystem {
@@ -32,7 +32,7 @@
             home-manager.extraSpecialArgs = {
               username = "warren";
               inherit dotfiles;
-              userChrome = builtins.readFile "${dotfiles.tridactyl}/.config/tridactyl/better-firefox-chrome.css";
+              userChrome = builtins.readFile "${dotfiles.packages.${system}.tridactyl}/.config/tridactyl/better-firefox-chrome.css";
               # Optionally, use home-manager.extraSpecialArgs to pass
               # arguments to home.nix
             };
