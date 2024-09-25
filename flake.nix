63a55c3082f7ce6e264cdee7b015b13016904495
Warren Harper <wharper@pyroninc.com>
2024-09-25 08:17:07 -0400

    Fix syntax problem

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    dotfiles.url = "sr.ht:warren/dotfiles/master"
   };
 
   outputs =
@@ -11,6 +12,7 @@
       self,
       nixpkgs,
       home-manager,
+      dotfiles,
       ...
     }@inputs:
     {
@@ -25,8 +27,8 @@
             home-manager.users.warren = import ./home/default.nix;
             home-manager.extraSpecialArgs = {
               username = "warren";
-            };
-
+              inherit dotfiles;
+              userChrome = builtins.readFile "${dotfiles.tridactyl}/.config/tridactyl/better-firefox-chrome.css"
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
           }
