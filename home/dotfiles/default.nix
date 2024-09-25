930d5ef4bfff9b7734671d4f39cfcac1e4a41f58
Warren Harper <wharper@pyroninc.com>
2024-09-25 08:10:25 -0400

    Fix syntax problem

--- home/dotfiles/default.nix
+++ home/dotfiles/default.nix
@@ -1,12 +1,13 @@
 # We need to pull in the dotfiles
-{dotfiles, ...}: {
+{ dotfiles, ... }:
+{
   home.file = {
     fish = {
       source = "${dotfiles.fish}/.config/fish"; # This should be path the fish config directory.
       target = ".config/fish";
       recursive = true;
     };
-    
+
     emacs = {
       source = "${dotfiles.emacs}/.emacs.d";
       target = ".emacs.d";
@@ -27,7 +28,7 @@
 
     wezterm = {
       source = "${dotfiles.wezterm}/.wezterm.lua";
-      target= ".wezterm.lua";
+      target = ".wezterm.lua";
     };
 
     tridactyl = {
@@ -35,5 +36,5 @@
       target = ".config/tridactyl";
       recursive = true;
     };
-  }
+  };
 }
