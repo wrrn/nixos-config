59c5e6f5152d4166aa2410d4d96821a7e0c299cc
Warren Harper <wharper@pyroninc.com>
2024-09-25 08:00:43 -0400

    Fix dnsmasq issues

--- dnsmasq.nix
+++ dnsmasq.nix
@@ -1,20 +1,20 @@
-{ config, pkgs, ...}:
+{ config, pkgs, ... }:
 let
-addresses = import ./blocked-addresses.nix;
-cname = builtins.map(addr: "${addr},localhost") addresses;
+  addresses = import ./blocked-addresses.nix;
+  cname = builtins.map (addr: "${addr},localhost") addresses;
 in
 {
 
-services.dnsmasq = {
-		 enable = true;
-		 alwaysKeepRunning = true;
-		 settings = {
-		 	    inherit address cname;
-			    server = [
-		 	    	   "1.1.1.3"
-				   "1.0.0.3"
-				   ];
-			    };
+  services.dnsmasq = {
+    enable = true;
+    alwaysKeepRunning = true;
+    settings = {
+      inherit cname;
+      server = [
+        "1.1.1.3"
+        "1.0.0.3"
+      ];
+    };
 
-};
+  };
 }
