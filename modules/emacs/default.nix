7158a2c2ca35daa00d46d6c0259c507846320034
Warren Harper <warren@wjh.io>
2024-10-09 19:28:28 -0400

    Enable service only if it's emacs

--- modules/emacs/default.nix
+++ modules/emacs/default.nix
@@ -7,17 +7,22 @@
 let
   inherit (inputs) dotfiles;
   inherit (config.device-conf) username;
-  emacsPackage = {
-    aarch64-darwin = pkgs.emacs-plus;
-    # Use the pure gtk version so that it works without xwayland
-    x86_64-linux = pkgs.emacs29-pgtk;
-  };
+  inherit (pkgs.stdenv) isLinux;
+  inherit (pkgs.hostPlatform) system;
+
+  emacsPackage =
+    {
+      aarch64-darwin = pkgs.emacs-plus;
+      # Use the pure gtk version so that it works without xwayland
+      x86_64-linux = pkgs.emacs29-pgtk;
+    }
+    ."${system}";
 in
 {
   # Start emacs-server with systemd
   nixpkgs.overlays = [ inputs.wrrnpkgs.overlay.macApps ];
   services.emacs = {
-    enable = true;
+    enable = isLinux;
     package = emacsPackage.${pkgs.hostPlatform.system};
   };
 
