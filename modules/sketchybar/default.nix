7911e4ab1ff34ccc4d9df996d18bf949c4328ca9
Warren Harper <warren@harperwarren.com>
2025-10-16 18:23:56 -0400

    Add sketchybar configuration

--- modules/sketchybar/default.nix
+++ modules/sketchybar/default.nix
@@ -1,5 +1,33 @@
-_: {
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
+let
+  inherit (inputs) dotfiles;
+  inherit (config.device-conf) username;
+in
+{
   services.sketchybar = {
-    enable = false;
+    enable = true;
+    extraPackages = [
+      lua5_4_compat
+      sbarlua
+    ];
+  };
+
+  nixpkgs.overlays = [ dotfiles.overlays.default ];
+  home-manager.users.${username} = {
+    home.packages = [
+      lua5_4_compat # We use lua to configure it
+      sbarlua # The lua library that we use to configure it
+    ];
+
+    home.file.dot-sketchybar = {
+      target = ".config/sketchybar";
+      source = "${pkgs.dotfiles.sketchybar}/.config/sketchybar";
+      recursive = true;
+    };
   };
 }
