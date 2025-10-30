bee99360a68349be2916d8cb189dd8f51ac0b734
Warren Harper <warren@harperwarren.com>
2025-10-30 18:40:21 -0400

    fix - remove mac-app-util. Copy Apps for now

--- modules/home-manager/darwin.nix
+++ modules/home-manager/darwin.nix
@@ -2,16 +2,23 @@
   inputs,
   lib,
   pkgs,
+  config,
   ...
 }:
 let
   inherit (inputs) mac-app-util;
   inherit (lib) mkIf;
   inherit (pkgs.stdenv) isDarwin;
-
+  inherit (config.device-conf) username;
 in
 mkIf isDarwin {
-  home-manager.sharedModules = [
-    mac-app-util.homeManagerModules.default
-  ];
+  home-manager.users.${username}.targets.darwin = {
+    copyApps.enable = true;
+    linkApps.enable = false;
+  };
+
+  ## Disable for now because sbcl is broken
+  # home-manager.sharedModules = [
+  #   mac-app-util.homeManagerModules.default
+  # ];
 }
