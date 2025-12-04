f0e8ffa641e3226823670964ab71b040b681c97b
Warren Harper <warren@wjh.io>
2025-12-04 13:49:44 -0500

    lona - set the system for vm

--- devices/lona/device.nix
+++ devices/lona/device.nix
@@ -1,12 +1,12 @@
 { flake-utils, nixpkgs, ... }:
 let
-  inherit (flake-utils.lib.system) x86_64-linux;
+  inherit (flake-utils.lib.system) x86_64-linux aarch64-linux;
 in
 {
   username = "warren";
   displayName = "Warren";
   hostname = "lona";
   nixOS.stateVersion = "25.11";
-  platform = nixpkgs.lib.systems.elaborate x86_64-linux;
+  platform = nixpkgs.lib.systems.elaborate aarch64-linux; # TODO: Change for laptop
   home-manager.stateVersion = "26.05";
 }
