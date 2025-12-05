9d01335189b978dc99d5c8618cfc42be2d1d3a66
Warren Harper <me@wrrn.org>
2025-12-05 12:59:43 -0500

    lona - Add build-tools and ssh

--- devices/lona/default.nix
+++ devices/lona/default.nix
@@ -30,6 +30,7 @@ in
     ../../modules/1password
     ../../modules/audio
     ../../modules/bluetooth
+    ../../modules/build-tools
     ../../modules/containers
     ../../modules/emacs
     ../../modules/email
@@ -45,16 +46,11 @@ in
     ../../modules/printing
     ../../modules/sddm
     ../../modules/shell
+    ../../modules/ssh
     ../../modules/sudo
     ../../modules/tailscale
     ../../modules/user
     ../../modules/zen-browser
-
-    {
-
-      # Enable the X11 windowing system.
-      services.xserver.enable = true;
-    }
   ];
 
   specialArgs = {
