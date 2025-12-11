ce60f51083afbaa16f63f20e035d0fc187462bcb
Warren Harper <me@wrrn.org>
2025-12-11 10:44:58 -0500

    Add clipboard

---
+++ modules/clipboard/default.nix
@@ -0,0 +1,11 @@
+{ device-conf, pkgs, ... }:
+let
+  inherit (device-conf) username;
+in
+{
+  home-manager.users.${username}.home.packages = with pkgs; [
+    cliphist
+    wl-clipboard
+    xdg-utils
+  ];
+}
