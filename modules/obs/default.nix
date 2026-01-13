e0c4cf87b11702235e250134c74dd274ef117976
Warren Harper <me@wrrn.org>
2026-01-13 11:46:42 -0500

    OBS - setup fixes

--- modules/obs/default.nix
+++ modules/obs/default.nix
@@ -3,12 +3,30 @@
   programs.obs-studio = {
     enable = true;
     enableVirtualCamera = true;
-
     plugins = with pkgs.obs-studio-plugins; [
-      wlrobs
       obs-backgroundremoval
       obs-pipewire-audio-capture
       obs-vaapi # optional AMD hardware acceleration
+      obs-composite-blur
+      droidcam-obs
     ];
   };
+
+  # xdg-desktop-portal is required for screen capture on Wayland
+  # niri specifically recommends xdg-desktop-portal-gnome for screencasting
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gnome # Provides ScreenCast portal for Niri
+      xdg-desktop-portal-gtk # Provides other portals (file chooser, etc.)
+    ];
+    config.common.default = [
+      "gnome"
+      "gtk"
+    ];
+  };
+
+  # Enable the usbmuxd so that iphones work.
+  services.usbmuxd.enable = true;
+  programs.droidcam.enable = true;
 }
