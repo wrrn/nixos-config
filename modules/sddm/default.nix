69fab6fe3a053764f7aa01607c7a9333e6414185
Warren Harper <me@wrrn.org>
2026-01-22 11:50:15 -0500

    sddm - Change it to ly

--- modules/sddm/default.nix
+++ modules/sddm/default.nix
@@ -1,15 +1,22 @@
 { pkgs, ... }:
-{
-  environment.systemPackages = [
-    (pkgs.where-is-my-sddm-theme.override ({
-      themeConfig = (import ./sddm.theme.nix { }).style;
-      variants = [ "qt5" ];
-    }))
-  ];
+# let
+#   customTheme = pkgs.where-is-my-sddm-theme.override {
+#     themeConfig = import ./sddm.theme.nix { };
+#     variants = [ "qt5" ];
+#   };
+# in
+# {
+#   environment.systemPackages = [ customTheme ];
 
-  services.displayManager.sddm = {
+#   services.displayManager.sddm = {
+#     enable = true;
+#     wayland.enable = true;
+#     theme = "${customTheme}/share/sddm/where_is_my_ssdm_theme_qt5";
+#     extraPackages = [ customTheme ];
+#   };
+# }
+{
+  services.displayManager.ly = {
     enable = true;
-    wayland.enable = true;
-    theme = "where_is_my_sddm_theme_qt5";
   };
 }
