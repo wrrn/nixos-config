a5054294cf6d8f2e3dca6a6b56fe3d87bee98d5f
Warren Harper <me@wrrn.org>
2026-02-11 09:50:34 -0500

    niri: order polkit-gnome after niri

--- modules/niri/polkit.nix
+++ modules/niri/polkit.nix
@@ -6,5 +6,8 @@ in
   # For adding auth when an app needs to sudo.
   security.polkit.enable = true;
 
-  home-manager.users.${username}.services.polkit-gnome.enable = true;
+  home-manager.users.${username} = {
+    services.polkit-gnome.enable = true;
+    systemd.user.services.polkit-gnome.Unit.After = [ "niri.service" ];
+  };
 }
