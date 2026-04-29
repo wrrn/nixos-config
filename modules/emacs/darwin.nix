d9a5a410010f68b725177e7db496f10d0567bd12
Warren Harper <me@wrrn.org>
2026-04-28 20:21:19 -0400

    emacs-client: Add

--- modules/emacs/darwin.nix
+++ modules/emacs/darwin.nix
@@ -1,16 +1,17 @@
-{device-conf, inputs, ...}:
+{ device-conf, inputs, ... }:
 let
   inherit (device-conf) username;
-   wrrnpkgs = inputs.wrrnpkgs.packages.${device-conf.platform.system};
+  wrrnpkgs = inputs.wrrnpkgs.packages.${device-conf.platform.system};
 in
 {
 
-    home-manager.users.${username} = {
-      services.emacs.package = wrrnpkgs.emacs-plus;
+  home-manager.users.${username} = {
+    services.emacs.package = wrrnpkgs.emacs-plus;
 
     programs.emacs = {
       package = wrrnpkgs.emacs-plus;
     };
 
-    };
+    home.packages = [ wrrnpkgs.emacs-plus-client ];
+  };
 }
