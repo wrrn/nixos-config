873a17cffcf1c7a63dc0ce090f1e17f2d5f7db0e
Warren Harper <me@wrrn.org>
2026-05-09 15:38:55 -0400

    ghostty: Setup terminfo for darwin

--- modules/ghostty/darwin.nix
+++ modules/ghostty/darwin.nix
@@ -1,9 +1,13 @@
-_: {
-  homebrew = {
-    enable = true;
-    casks = [
-      "ghostty"
-    ];
-  };
+{ device-conf, pkgs, ... }:
+let
+  inherit (device-conf) username;
+in
+{
+  environment.systemPackages = [
+    pkgs.unstable.ghostty-bin
+    pkgs.unstable.ghostty-bin.terminfo
+  ];
 
+  home-manager.users.${username}.home.file.".terminfo/78/xterm-ghostty".source =
+    "${pkgs.unstable.ghostty-bin.terminfo}/share/terminfo/78/xterm-ghostty";
 }
