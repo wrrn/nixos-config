4826a72f94cffe8e1ff1e4b7e3ddb304ed2b7c59
Warren Harper <me@wrrn.org>
2026-07-31 10:39:27 -0400

    power: only charge to 80%

--- modules/power/default.nix
+++ modules/power/default.nix
@@ -1,9 +1,29 @@
-_: {
+let
+  batteryPath = "/sys/class/power_supply/BAT0";
+in
+{
   services.power-profiles-daemon.enable = true;
 
   services.logind.settings.Login = {
     HandlePowerKey = "poweroff";
     HandleLidSwitch = "suspend";
     # HandleLidSwitchExternalPower = "ignore";
+  };
+
+  systemd.services.battery-charge-limit = {
+    description = "Configure battery charge thresholds";
+    wantedBy = [ "multi-user.target" ];
+    unitConfig.ConditionPathExists = "${batteryPath}/charge_types";
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+
+    script = ''
+      echo 50 > ${batteryPath}/charge_control_start_threshold
+      echo 80 > ${batteryPath}/charge_control_end_threshold
+      echo Custom > ${batteryPath}/charge_types
+    '';
   };
 }
