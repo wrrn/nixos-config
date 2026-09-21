e16b1295c0a39e69551c5eb1898132e423a46909
Warren Harper <me@wrrn.org>
2026-09-21 15:26:25 -0400

    claude-code: re-add

--- modules/llms/default.nix
+++ modules/llms/default.nix
@@ -6,6 +6,10 @@
   ...
 }:
 let
+  inherit (device-conf) username;
+  inherit (device-conf.platform) system;
+  wrrnpkgs = inputs.wrrnpkgs.packages.${system};
+
   module = lib.systemModule {
     linux = ./linux.nix;
     darwin = ./darwin.nix;
@@ -13,4 +17,7 @@ let
 in
 {
   imports = [ module ];
+  home-manager.users.${username}.home.packages = [
+    wrrnpkgs.claude-code
+  ];
 }
